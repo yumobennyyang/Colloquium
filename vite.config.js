bf1a9d86f79f85327ab931b9133586eeef1ce4c9
yumobennyyang <123268757+yumobennyyang@users.noreply.github.com>
2025-08-11 23:22:05 -0400

    try

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Colloquium/', // Change this to your actual repository name
+  base: '/', // Change this to your actual repository name
   build: {
     outDir: 'dist',
   }
